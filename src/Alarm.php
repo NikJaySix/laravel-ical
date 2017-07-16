3e2549a8592e16c35535b765e00486832f85c07d
Nicole Jay <hello@nikjaysix.com.au>
2017-07-16 12:31:25 +1000

    add alarm class

---
+++ src/Alarm.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: nik
+     * Date: 16/7/17
+     * Time: 12:30 PM
+     */
+
+    namespace NikJaySix\LaravelIcal;
+
+
+    use Eluceo\iCal\Component\Alarm as CalendarAlarm;
+
+    class Alarm extends CalendarAlarm
+    {
+
+    }
